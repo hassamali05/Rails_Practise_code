5c8b4fc003cbb17de14e6bb04d9dc4f249a4e691
Hassam Ali <hassamali05@gmail.com>
2019-07-15 11:30:00 +0500

    Just create user model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_13_104316) do
+ActiveRecord::Schema.define(version: 2019_07_15_062249) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_07_13_104316) do
     t.string "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
